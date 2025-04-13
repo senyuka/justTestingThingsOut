09e905cd0adc7c9badb2ed1e79497bc69e3ac3b0
artemiy <art.senyuk@gmail.com>
2025-04-13 23:40:53 +0200

    ACTUAL commit

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-print("hello world! oh wait like that is better")
-print("i actually want it like that")
-=======
 print("hello world! yep now it's definitely correct")
 print("i actually want it like that")
->>>>>>> UpdateText
